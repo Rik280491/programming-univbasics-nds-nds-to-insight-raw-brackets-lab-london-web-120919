62dc54954850bba62fd42a97c01c2202df73079f
Rikesh Balakrishnan <rik280491@googlemail.com>
2019-11-22 15:40:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,18 +6,4 @@ def directors_totals(nds)
    result = {
   }
 
-total_gorss = 0 
-row_index = 0 
-while row_index < directors_database.length do 
-  directors = directors_database[row_index][:name]
-  movies = directors_database[row_index][:movies]
-  inner_index = 0 
-  while inner_index < movies.length do 
-    total_gross += movies[inner_index][:worldwide_gross]
-    result << directors
-    result << total_gross 
-    inner_index += 1 
-    end 
-    row_index += 1 
- end 
 
